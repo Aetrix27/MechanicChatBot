ce3407ea13c0e0c9f197fa8e11b7589d9a118db7
Aetrix27 <davedogg323@gmail.com>
2026-06-27 21:57:48 -0700

    Removed deprecated tool

--- tools.py
+++ tools.py
@@ -1,3 +1,4 @@
+'''
 from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
@@ -28,3 +29,4 @@ search_tool = Tool(
 
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
 wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+'''
